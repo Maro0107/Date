607d51268c99661bd0191611adbebdaec68a0bfd
Marek Fijalkowski <gitmaro1983@gmail.com>
2022-10-12 19:34:34 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+        System.out.println(date.toString());
+
+        // Formatowanie daty
+// y - rok jako liczby np 2021
+// M - miesiąc w roku np. Jan czy 01
+// d - dzień np. 10
+// h - godzina (1-12)  np. 6
+// H - godzina (0-23)  np. 15
+// m - minuta np 45
+// s - sekunda np 56
+// S - milisekunda np 456
+// E - dzień tygodnia np pon.
+// D - dzień roku np 123
+// w - numer tygodnia w roku np 3
+// W - numer tygodnia w miesiącu np 2
+// a - A.M. / P.M.   np PM
+// z - time zone np Eastern Standard Time
+        
+        SimpleDateFormat simpleDate = new SimpleDateFormat("E dd.MM.YYYY");
+        System.out.println(simpleDate.format(date));
+    }
+}
